ed9d017f3039cb96ea8086b6479f8d1f8c231a9b
Swarup Ghorsaine <ghorsaineswarup25@gmail.com>
2026-06-23 16:29:12 +0545

    Update config.server.ts

--- Frontend/src/lib/config.server.ts
+++ Frontend/src/lib/config.server.ts
@@ -24,10 +24,3 @@ export function getServerConfig() {
     //   stripeSecretKey: process.env.STRIPE_SECRET_KEY,
   };
 }
-// update 1
-// update 2
-// update 3
-// update 4
-// update 5
-// update 6
-// update 7
